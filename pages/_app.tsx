990dd4db91bf721560bb2ac49a85756e51418ef6
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-11 11:26:18 -0500

    add chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
-import { ChakraProvider } from '@chakra-ui/core'
+import {
+	ChakraProvider,
+	ColorModeProvider,
+	Container,
+	cookieStorageManager,
+	extendTheme,
+	localStorageManager,
+} from '@chakra-ui/core'
+import nookies from 'nookies'
 import AuthProvider from '../src/auth/context'
 
 const App = ({ Component, pageProps }) => (
-	<ChakraProvider>
-		<AuthProvider>
-			<Component {...pageProps} />
-		</AuthProvider>
+	<ChakraProvider colorModeManager={localStorageManager}>
+		<ColorModeProvider
+			options={{
+				initialColorMode: 'light',
+			}}>
+			<AuthProvider>
+				<Container>
+					<Component {...pageProps} />
+				</Container>
+			</AuthProvider>
+		</ColorModeProvider>
 	</ChakraProvider>
 )
 
